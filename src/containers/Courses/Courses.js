7cc3a14388be1d130cac8eaf835fd0930f8c8918
Katarzyna-Maryanska <kmaryanska@gmail.com>
2019-07-24 19:26:50 +0200

    add routing

--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import {Link, Route} from "react-router-dom";
 
 import './Courses.css';
+import Course from "../Course/Course";
 
 class Courses extends Component {
     state = {
@@ -9,7 +11,7 @@ class Courses extends Component {
             { id: 2, title: 'Vue - The Complete Guide' },
             { id: 3, title: 'PWA - The Complete Guide' }
         ]
-    }
+    };
 
     render () {
         return (
@@ -18,10 +20,17 @@ class Courses extends Component {
                 <section className="Courses">
                     {
                         this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
-                        } )
+                            return (
+                                <Link
+                                    key={course.id}
+                                    to={this.props.match.url + "/" + course.id + "/" + course.title}>
+                                    <article className="Course" >{course.title}</article>
+                                </Link>
+                            )
+                        })
                     }
                 </section>
+                <Route path={this.props.match.url + "/:courseId"} component={Course}/>
             </div>
         );
     }
